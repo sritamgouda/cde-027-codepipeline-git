856ae63e64e8ca53197880938161f49e0085aa85
sritamgouda <sritamgouda1@gmail.com>
2022-02-23 12:08:42 +0530

    Post mapping added

--- src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplication.java
+++ src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplication.java
@@ -3,6 +3,8 @@ package com.ms.codepipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,6 +19,12 @@ public class CdeAwsCodepipelineGitApplication {
    public String welcome()
    {
 	   return"code pipeline";
+   }
+   
+   @PostMapping("/hello")
+   public String sayhello(@RequestBody String name)
+   {
+	   return name+"welcome to codepipeline ";
    }
 	public static void main(String[] args) {
 		SpringApplication.run(CdeAwsCodepipelineGitApplication.class, args);
